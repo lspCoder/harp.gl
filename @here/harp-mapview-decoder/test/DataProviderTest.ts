178a2d2eeb04a3bcb39d96cc488b5226bf3c5979
stefan.dachwitz <stefan.dachwitz@here.com>
2020-11-05 16:29:49 +0100

    HARP-12352: subsequent DataProvider.register() returns early
    * subsequent register operations should resolve after first connect
    
    Signed-off-by: stefan.dachwitz <stefan.dachwitz@here.com>

--- @here/harp-mapview-decoder/test/DataProviderTest.ts
+++ @here/harp-mapview-decoder/test/DataProviderTest.ts
@@ -29,6 +29,17 @@ class FakeDataProvider extends DataProvider {
     }
 }
 
+class TimedFakeDataProvider extends FakeDataProvider {
+    /** @override */ async connect(): Promise<void> {
+        const promise = new Promise<void>(resolve => {
+            setTimeout(() => {
+                resolve();
+            }, 100);
+        });
+        return promise;
+    }
+}
+
 describe("DataProvider", function() {
     const clients = [{}, {}];
 
@@ -59,5 +70,45 @@ describe("DataProvider", function() {
         expect(disposeSpy.calledOnce).is.true;
         dataProvider.unregister(clients[1]);
         expect(disposeSpy.calledOnce).is.true;
+    });
+
+    it("calls connect only on first registered client", async function() {
+        const dataProvider = new TimedFakeDataProvider();
+        const connectSpy = sinon.spy(dataProvider, "connect");
+
+        await dataProvider.register(clients[0]);
+        expect(connectSpy.calledOnce).is.true;
+
+        await dataProvider.register(clients[0]);
+        await dataProvider.register(clients[1]);
+        expect(connectSpy.calledOnce).is.true;
+    });
+
+    it("calls connect only on first registered client", async function() {
+        const dataProvider = new TimedFakeDataProvider();
+        const connectSpy = sinon.spy(dataProvider, "connect");
+
+        dataProvider.register(clients[0]);
+        dataProvider.register(clients[1]);
+        expect(connectSpy.calledOnce).is.true;
+    });
+
+    it("subsequent register operations wait for connect", async function() {
+        const dataProvider = new TimedFakeDataProvider();
+        const connectSpy = sinon.spy(dataProvider, "connect");
+
+        const promise0 = dataProvider.register(clients[0]);
+        const promise1 = dataProvider.register(clients[1]);
+
+        let promise0Resolved = false;
+
+        promise0.then(() => {
+            promise0Resolved = true;
+        });
+
+        await promise1;
+
+        expect(promise0Resolved, "subsequent promises resolve before the first").to.be.true;
+        expect(connectSpy.calledOnce).is.true;
     });
 });
